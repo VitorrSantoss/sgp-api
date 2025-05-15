ef72a02b506c0013146e6a1b49dfae70a09a91fd
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-05-14 23:30:14 -0300

    Estrutura para a consulta com o banco de dados criada

--- src/main/java/com/sgp/api/repository/UsuarioRepository.java
+++ src/main/java/com/sgp/api/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.sgp.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sgp.api.model.Usuario;
 
+// CONSULTA AO BANCO DE DADOS (conexão com o banco de dados)
+
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
   
 }
