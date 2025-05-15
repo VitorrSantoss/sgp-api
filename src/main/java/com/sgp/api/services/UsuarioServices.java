4bacce6c11a5dd2178eecc86e464627b99ade308
Vitor Santos <Vitorvitorsantos2015@hotmail.com>
2025-05-14 23:35:48 -0300

    estrutua de consulta e delete do UsuarioServices

--- src/main/java/com/sgp/api/services/UsuarioServices.java
+++ src/main/java/com/sgp/api/services/UsuarioServices.java
@@ -1,5 +1,32 @@
 package com.sgp.api.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sgp.api.model.Usuario;
+import com.sgp.api.repository.UsuarioRepository;
+
+// REGRA DE NEGÓCIO DO PROJETO
+
+@Service
 public class UsuarioServices {
   
+  public List<Usuario> carregarUsuarioCadastrado(){
+    // To Do: Lógica adicional (ex: calcular e retornar idade)
+    return usuarioRepository.findAll(); // Select * From tb_usuarios
+  }
+
+  public Optional<Usuario> obterDadosUsuarioPeloId(Long id){
+    return usuarioRepository.findById(id);
+  }
+
+  public void excluirUsuario(Long id){
+    usuarioRepository.deleteById(id);
+  }
+
+  @Autowired
+  private  UsuarioRepository usuarioRepository;
 }
